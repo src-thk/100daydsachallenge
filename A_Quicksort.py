818eacadc638bf9c265a10ad360d1e28e3147f04
src-thk <sarthak.tandon@outlook.com>
2022-07-19 00:01:17 +0530

    DAY03: Quicksort Algo

---
+++ A_Quicksort.py
@@ -0,0 +1,18 @@
+def quick_sort(arr, l, r):
+    if l >= r:
+        return
+    p = partition(arr, l, r)
+    quick_sort(arr, l, p - 1)
+    quick_sort(arr, p + 1, r)
+
+
+def partition(arr, l, r):
+    i = l - 1
+    pivot = arr[r]
+
+    for j in range(l, r):
+        if arr[j] < pivot:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[r] = arr[r], arr[i + 1]
+    return i + 1
